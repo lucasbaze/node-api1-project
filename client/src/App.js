b312956ab606a7a5a68b7cfdc9053dd68f6a592a
Lucas Bazemore <lucasabazemore@gmail.com>
2019-09-30 23:01:34 -0500

    finished stretch

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [users, setUsers] = useState();
+
+    useEffect(() => {
+        let url = 'http://localhost:8080/api/users';
+        axios
+            .get(url)
+            .then(data => setUsers(data.data))
+            .catch(err => console.log(err));
+    }, []);
+
+    const deleteUser = id => {
+        axios.delete(`http://localhost:8080/api/users/${id}`).then(res => {
+            console.log(res);
+            let filtered = users.filter(user => {
+                return user.id != res.data;
+            });
+            setUsers(filtered);
+        });
+    };
+
+    return (
+        <div className="App">
+            <h1>Users</h1>
+            {users &&
+                users.map((user, i) => {
+                    return (
+                        <div key={i}>
+                            <h1>{user.name}</h1>
+                            <div>{user.bio}</div>
+                            <button onClick={() => deleteUser(user.id)}>
+                                Delete
+                            </button>
+                        </div>
+                    );
+                })}
+        </div>
+    );
 }
 
 export default App;
